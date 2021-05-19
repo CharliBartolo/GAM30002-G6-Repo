2fda421a29e9f7dfa592c8bb548a034755b44020
Matt Smith <102088891@student.swin.edu.au>
2021-05-19 13:44:58 +1000

    Added more GFX controllers

--- HotAndColdGame/Assets/Scripts/FXControllers/MachineFXController.cs
+++ HotAndColdGame/Assets/Scripts/FXControllers/MachineFXController.cs
@@ -11,6 +11,7 @@ public class MachineFXController : FXController
 
     // Machine emissive components
     public GameObject[] emissiveLights;
+    public float emissionValue;
 
 
     // Start is called before the first frame update
@@ -43,18 +44,21 @@ public class MachineFXController : FXController
                 foreach (var item in emissiveLights)
                 {
                     item.GetComponent<Renderer>().sharedMaterial.color = Crystal_Cold;
+                    item.GetComponent<Renderer>().sharedMaterial.SetColor("_EmissiveColor", Crystal_Cold * emissionValue);
                 }
                 break;
             case ITemperature.tempState.Neutral:
                 foreach (var item in emissiveLights)
                 {
                     item.GetComponent<Renderer>().sharedMaterial.color = Crystal_Neutral;
+                    item.GetComponent<Renderer>().sharedMaterial.SetColor("_EmissiveColor", Crystal_Neutral * 0f);
                 }
                 break;
             case ITemperature.tempState.Hot:
                 foreach (var item in emissiveLights)
                 {
                     item.GetComponent<Renderer>().sharedMaterial.color = Crystal_Hot;
+                    item.GetComponent<Renderer>().sharedMaterial.SetColor("_EmissiveColor", Crystal_Hot * emissionValue);
                 }
                 break;
         }
