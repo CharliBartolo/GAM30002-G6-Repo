157c83b5b6db6bf8f6b06b7e4344b689a00bc525
Jason8664 <102102775@student.swin.edu.au>
2021-05-26 16:41:03 +1000

    Player Taking too long to return to Neutral Temp

--- HotAndColdGame/Assets/Scripts/PlayerTemperature.cs
+++ HotAndColdGame/Assets/Scripts/PlayerTemperature.cs
@@ -5,15 +5,28 @@ using UnityEngine;
 
 public class PlayerTemperature : TemperatureStateBase
 {
-    //Variables
-    [SerializeField]
-    private float _valueChangeSpeed; //The speed to change the value of the slider
 
     override protected void Start()
     {
-        base.Start();
-        _valueChangeSpeed = 10f;  //Set the speed of change
+        base.Start();        
         powerDownRateInSeconds = 15f;  
+    }
+
+    public override void PowerDownToNeutral(float tempCap)
+    {
+        float min = tempValueRange[0];  //cold -100f
+        float max = tempValueRange[2];  //hot 100f
+        float mid = tempValueRange[1];  //neutral 0
+
+        //debug for testing purposes
+        //Debug.Log("crnt" + currentTemp);
+
+        currentTemp = Mathf.Lerp(currentTemp, mid, Time.deltaTime);
+
+        if (tempValueRange[1] < tempCap)
+            currentTemp = Mathf.Clamp(currentTemp, tempValueRange[1], tempCap);
+        else
+            currentTemp = Mathf.Clamp(currentTemp, tempCap, tempValueRange[1]);       
     }
 
 
@@ -24,30 +37,6 @@ public class PlayerTemperature : TemperatureStateBase
     /// <param name="temperature"></param> //Passed through from the PlayerUI script
     public void ValueChange(Slider slider, string temperature)
     {
-        slider.value = -currentTemp;
-        // switch (temperature)
-        // {
-        //     //Cold
-        //     case "Cold":
-        //         slider.value += _valueChangeSpeed *  Time.deltaTime;
-        //         break;
-
-        //     //Hot
-        //     case "Heat":
-        //         slider.value -= _valueChangeSpeed * Time.deltaTime;
-        //         break;
-
-        //     //Neutral (Possible to clamp the value to exact value)
-        //     case "Neutral":
-        //         if (slider.value >= slider.maxValue / 2)
-        //         {
-        //             slider.value -= _valueChangeSpeed * Time.deltaTime;
-        //         }
-        //         else if (slider.value <= slider.maxValue / 2)
-        //         {
-        //             slider.value += _valueChangeSpeed * Time.deltaTime;
-        //         }
-        //         break;
-        // }
+        slider.value = -currentTemp;//for slider
     }
 }
