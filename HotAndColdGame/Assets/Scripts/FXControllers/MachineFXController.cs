506675c300afe2ea9dfa899e00cc35ab09e90e7e
Matt Smith <102088891@student.swin.edu.au>
2021-05-30 16:29:43 +1000

    Added sound fx to machine

--- HotAndColdGame/Assets/Scripts/FXControllers/MachineFXController.cs
+++ HotAndColdGame/Assets/Scripts/FXControllers/MachineFXController.cs
@@ -39,29 +39,89 @@ public class MachineFXController : FXController
         base.PerformFX();
 
         SetEmissiveLights();
-        if(Math.Abs(machine.CurrentTemperature)>5)
+        PlaySound();
+
+
+    }
+
+    public void PlaySound()
+    {
+        // have pitch match current temperature
+        machine.GetComponent<AudioSource>().pitch = (float)Math.Abs((machine.CurrentTemperature) * 0.1);
+
+        if (Math.Abs(machine.CurrentTemperature) > 5)
         {
-            if(!playingSFX)
+            if (!playingSFX)
             {
                 playingSFX = true;
                 GameMaster.instance.audioManager.Play(machine.gameObject);
             }
-           
+
         }
-        if(playingSFX)
+        else
         {
-            if(Math.Abs(machine.CurrentTemperature) < 5)
+            playingSFX = false;
+            GameMaster.instance.audioManager.Stop(machine.gameObject);
+        }
+        /*if (playingSFX)
+        {
+            // stop sound when reach max
+            if(Math.Abs(machine.CurrentTemperature) == 100)
             {
                 playingSFX = false;
                 GameMaster.instance.audioManager.Stop(machine.gameObject);
             }
-        }
+
+            
+            if (Math.Abs(machine.CurrentTemperature) < 5)
+            {
+                playingSFX = false;
+                GameMaster.instance.audioManager.Stop(machine.gameObject);
+            }
+        }*/
+    }
+
+    public void StartSound()
+    {
+
+    }
+    public void StopSound()
+    {
+
     }
 
     // set emmisive lights
     void SetEmissiveLights()
     {
         // set crystals colour to neutral if not shooting or cannot shoot
+
+        /*if (machine.CurrentTemperature < -5)
+        {
+            foreach (var item in this.emissiveLights)
+            {
+                item.GetComponent<Renderer>().sharedMaterial.color = Crystal_Cold;
+                item.GetComponent<Renderer>().sharedMaterial.SetColor("_EmissiveColor", Crystal_Cold * emissionValue * (Math.Abs(machine.CurrentTemperature)));
+            }
+        }
+        else if (machine.CurrentTemperature > 5)
+        {
+            foreach (var item in this.emissiveLights)
+            {
+                item.GetComponent<Renderer>().sharedMaterial.color = Crystal_Hot;
+                item.GetComponent<Renderer>().sharedMaterial.SetColor("_EmissiveColor", Crystal_Hot * emissionValue * (Math.Abs(machine.CurrentTemperature)));
+            }
+        }
+        else
+        {
+            foreach (var item in this.emissiveLights)
+            {
+                item.GetComponent<Renderer>().sharedMaterial.color = Crystal_Neutral;
+                item.GetComponent<Renderer>().sharedMaterial.SetColor("_EmissiveColor", Crystal_Neutral * 0f);
+            }
+        }*/
+
+
+
         switch (machine.CurrentTempState)
         {
             case ITemperature.tempState.Cold:
