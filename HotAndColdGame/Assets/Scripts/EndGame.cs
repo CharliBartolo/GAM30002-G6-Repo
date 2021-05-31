926bad27db9a5c1e62aeb4ff1db98bdcdb8c7808
Matt Smith <102088891@student.swin.edu.au>
2021-05-31 16:40:33 +1000

    added endgame scene

--- HotAndColdGame/Assets/Scripts/EndGame.cs
+++ HotAndColdGame/Assets/Scripts/EndGame.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour
+public class EndGame : MonoBehaviour
 {
     // Start is called before the first frame update
-    public void Play()
+    public void GoToMainMenu()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        SceneManager.LoadScene(0);
     }
 }
