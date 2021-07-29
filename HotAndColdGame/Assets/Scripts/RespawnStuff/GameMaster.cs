efc4b0d36c6027c0e4fe5863bf566b4c49098914
Matt Smith <102088891@student.swin.edu.au>
2021-07-30 01:19:13 +1000

    added basic intro scene setup

--- HotAndColdGame/Assets/Scripts/RespawnStuff/GameMaster.cs
+++ HotAndColdGame/Assets/Scripts/RespawnStuff/GameMaster.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameMaster : MonoBehaviour
 {
@@ -21,5 +22,10 @@ public class GameMaster : MonoBehaviour
         {
             Destroy(gameObject);
         }
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
