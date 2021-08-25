1c96d39e50c603b179c487736e9aeb1e4248a10a
Matt Smith <102088891@student.swin.edu.au>
2021-08-25 15:20:12 +1000

    connected death effect to colour palette

--- HotAndColdGame/Assets/Scripts/DeathEffect.cs
+++ HotAndColdGame/Assets/Scripts/DeathEffect.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class DeathEffect : MonoBehaviour
+public class DeathEffect : FXController
 {
     [SerializeField] public Image Fade;//for image wanted on vignette    
     [SerializeField] public TemperatureStateBase crntTemp;//get player temperature
@@ -14,6 +14,7 @@ public class DeathEffect : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        base.Start();
         // makes sure death effect is invisible until function
         Fade.canvasRenderer.SetAlpha(0.0f);   
     }
@@ -35,19 +36,23 @@ public class DeathEffect : MonoBehaviour
                 case ITemperature.tempState.Hot: //if hot
                     //testing purposes
                     //Debug.Log("Player is overheating");
-                    Fade.GetComponent<Image>().color = new Color32(255, 0, 0, 100);//red, green, blue, alpha
+                    //Fade.GetComponent<Image>().color = new Color32(255, 0, 0, 100);//red, green, blue, alpha
+                    Fade.GetComponent<Image>().color = Crystal_Hot;//red, green, blue, alpha
                     Fade.CrossFadeAlpha(1, 3, false); //Fully fade Image with the duration of 3 seconds
                     StartCoroutine("ResetPlayer", 3f);                 
                     break;
                 case ITemperature.tempState.Cold://if cold
                     //testing purposes
                     //Debug.Log("Player is freezing");
-                    Fade.GetComponent<Image>().color = new Color32(0, 200, 255, 100);//red, green, blue, alpha
+                    //Fade.GetComponent<Image>().color = new Color32(0, 200, 255, 100);//red, green, blue, alpha
+                    Fade.GetComponent<Image>().color = Crystal_Cold; ;//red, green, blue, alpha
                     Fade.CrossFadeAlpha(1, 3, false); //Fully fade Image with the duration of 3 seconds
                     StartCoroutine("ResetPlayer", 3f);     
                     break;
                 default:
-                    Fade.GetComponent<Image>().color = new Color32(0, 0, 0, 100);
+                    //Fade.GetComponent<Image>().color = new Color32(0, 0, 0, 100);
+                    //Fade.GetComponent<Image>().color = Crystal_Neutral;
+                    Fade.canvasRenderer.SetAlpha(0.0f);
                     break;
             }
         }       
