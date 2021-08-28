dca5c9e212e2e65a0f428a5a292c2359e745ac2f
Charli <charlib33@gmail.com>
2021-08-28 16:06:59 +1000

    Updated ColourPallete.cs to allow for saving and loading of colour palletes

---
+++ HotAndColdGame/Assets/Editor/ColourPalleteInspector.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(ColourPalette))]
+public class ColourPalleteInspector : Editor
+{
+    // Start is called before the first frame update
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        ColourPalette col = (ColourPalette)target;
+        
+        if (GUILayout.Button("Save Colour Preferences to Slot 1"))
+        {
+            col.SaveColourPrefs(1);
+        }
+        
+        if (GUILayout.Button("Save Colour Preferences to Slot 2"))
+        {
+            col.SaveColourPrefs(2);
+        }
+
+        if (GUILayout.Button("Load Colour Preferences to Slot 1"))
+        {
+            col.LoadColourPrefs(1);
+        }
+
+        if (GUILayout.Button("Load Colour Preferences to Slot 2"))
+        {
+            col.LoadColourPrefs(2);
+        }
+    }
+}
