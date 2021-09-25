76a2d55affe4f55863a331a615a7011a7f6c21ec
charadey_kong <kongc28@gmail.com>
2021-09-25 16:53:40 +1000

    Tutorial (Direction Option addition) done!, won't push yet

--- HotAndColdGame/Assets/Scripts/TutorialPrompt.cs
+++ HotAndColdGame/Assets/Scripts/TutorialPrompt.cs
@@ -4,8 +4,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
+using UnityEditor;
 
-
+/// <summary>
+/// This script is responsible for creating and managing tutorial prompts placed in the scene.
+/// Note: Must be placed on the Player object!!!
+/// Last edited by Charadey 25/9/2001
+/// </summary>
 public class TutorialPrompt : MonoBehaviour
 {
     /*
@@ -60,6 +65,19 @@ public class TutorialPrompt : MonoBehaviour
         [Tooltip("True if the player has completed the Tutorial Prompt")]
         private bool _isAchieved;
 
+        //Vision additions
+        [SerializeField]
+        [Tooltip("If true, the must look in a certain direction to activate the Tutorial Prompt. (Mainly for debug)")]
+        private bool _needsVision;
+
+        [SerializeField]
+        [Tooltip("The minimum angle from the expected direction. -1 is behind, 0 is perpendicular and 1 is same direction")]
+        [Range (-1, 1)]
+        private float _visionWindow;
+
+        [SerializeField]
+        [Tooltip("Vector 3 for looking into the correct direction.")]
+        private Vector3 _expectedDirection;
         public string Label
         {
             get => _label;
@@ -81,7 +99,8 @@ public class TutorialPrompt : MonoBehaviour
             set => _endTrigger = value;
         }
 
-        /*public PlayerActions PlayerAction
+        /*
+        public PlayerActions PlayerAction
         {
             get => _playerAction;
             set => _playerAction = value;
@@ -106,7 +125,25 @@ public class TutorialPrompt : MonoBehaviour
             set => _isAchieved = value;
         }
 
-        public Prompt(string label, string message, Collider startTrigger, Collider endTrigger, /*PlayerActions playerAction,*/ bool active, /*bool isShowingPrompt,*/ bool achieved)
+        //Vision Additions
+        public bool NeedsVision
+        {
+            get => _needsVision;
+            set => _needsVision = value;
+        }       
+
+        public float VisionWindow
+        {
+            get => _visionWindow;
+            set => _visionWindow = value;
+        }
+        public Vector3 ExpectedDirection
+        {
+            get => _expectedDirection;
+            set => _expectedDirection = value;
+        }
+
+        public Prompt(string label, string message, Collider startTrigger, Collider endTrigger, /*PlayerActions playerAction,*/ bool active, /*bool isShowingPrompt,*/ bool achieved, bool vision, float angle, Vector3 expectedDirection)
         {
             this._label = label;
             this._message = message;
@@ -116,6 +153,21 @@ public class TutorialPrompt : MonoBehaviour
             this._isActive = active;
             //this._isShowingPrompt = isShowingPrompt;
             this._isAchieved = achieved;
+
+            //Vision Addition
+            this._needsVision = vision;
+
+            if (vision)
+            {
+                this._visionWindow = angle;
+                this._expectedDirection = expectedDirection;
+            }
+            else
+            {
+                this._visionWindow = 0f;
+                this._expectedDirection = new Vector3();
+            }
+            
         }
     }
 
@@ -143,7 +195,6 @@ public class TutorialPrompt : MonoBehaviour
         }
     }
 
-
     [SerializeField]
     private List<Prompt> _listTutorialPrompts;
     public List<Prompt> ListTutorialPrompts
@@ -158,12 +209,14 @@ public class TutorialPrompt : MonoBehaviour
         get => _queueTutorialPrompts;
         set => _queueTutorialPrompts = value;
     }
+
     private Prompt _currentPrompt;
     public Prompt CurrentPrompt
     {
         get => _currentPrompt;
         set => _currentPrompt = value;
     }
+
     private InputAction _currentAction;
     public InputAction CurrentAction
     {
@@ -182,10 +235,25 @@ public class TutorialPrompt : MonoBehaviour
     public bool IsCompleted;
 
     [Header("References")]
-    public PlayerInput PlayerInputs;
+    public GameObject Player;
+    //public PlayerInput PlayerInputs;
 
     private void Awake()
     {
+        //Player object
+        if (!Player)
+        {
+            Player = GameObject.Find("Player");
+        }
+
+        /*
+        //Player Inputs
+        if (Player != null && !PlayerInputs)
+        {
+            PlayerInputs = Player.GetComponent<PlayerInput>();
+        }
+        */
+
         //Initalise collections
         //_listTutorialPrompts = new List<Prompt>();
         _queueTutorialPrompts = new Queue<Prompt>();
@@ -207,31 +275,59 @@ public class TutorialPrompt : MonoBehaviour
             _dictPlayerActions.Add(PlayerActions.Walk, PlayerInputs.currentActionMap.FindAction("Walk"));
         }
         */
+
         //Updates the starting queue
         ListToQueueTransfer(_listTutorialPrompts, _queueTutorialPrompts); 
 
         //Gets the first Prompt object
         _currentPrompt = _queueTutorialPrompts.Peek();
+
+        _textTutorialPrompt.enabled = _currentPrompt.IsActive;
+        _imageTutorialPrompt.enabled = _currentPrompt.IsActive;
     }
 
     private void FixedUpdate()
     {
+        //Checks if all Tutorial Prompts have been completed by the player
         if (!IsCompleted)
         {
             //Checks if queue isn't empty
             if (_queueTutorialPrompts.Count != 0)
             {
-                //Toggles Tutorial Prompt Text based on the bool IsActive
-                _textTutorialPrompt.enabled = _currentPrompt.IsActive;
-                _imageTutorialPrompt.enabled = _currentPrompt.IsActive;
-                
-                //When the Prompt object has outlived its usefulness
-                if (_currentPrompt.IsAchieved)
-                {                  
-                    _listTutorialPrompts.RemoveAt(0); //Removes said Prompt object from the list                   
-                    ListToQueueTransfer(_listTutorialPrompts, _queueTutorialPrompts); //Updates the Queue
+                //Checks if player needs to look at a certain direction.
+                if (_currentPrompt.NeedsVision)
+                {
+                    //Finds and normalizes the needed vectors.
+                    var direction = Vector3.Normalize(Player.transform.TransformDirection(Vector3.forward));    //Players looking direction
+                    var expectedDirection = Vector3.Normalize(_currentPrompt.StartTrigger.transform.position - Player.transform.position);  //Vector from startTrigger to player
+                    var dotProduct = Vector3.Dot(_currentPrompt.ExpectedDirection, direction);  //Dot product of above vectors
 
-                    //
+                    //Debug                 
+                    //Debug.Log("Player Vector3: " + direction);
+                    //Debug.Log("Dot product: " + dotProduct + "/n Vision Window: " + CurrentPrompt.VisionWindow); 
+
+                    _currentPrompt.ExpectedDirection = expectedDirection;
+
+                    //Tests if the player is looking at the expected direction within the acceptable angle.
+                    if (dotProduct >= _currentPrompt.VisionWindow && _currentPrompt.IsActive)
+                    {
+                        //Toggles Tutorial Prompt Text based on the bool IsActive
+                        _textTutorialPrompt.enabled = _currentPrompt.IsActive;
+                        _imageTutorialPrompt.enabled = _currentPrompt.IsActive;
+                    }
+                }             
+                
+                //When the Prompt object has outlived its usefulness it returns nothingness
+                if (_currentPrompt.IsAchieved)
+                {
+                    //Turn off text and image
+                    _textTutorialPrompt.enabled = _currentPrompt.IsActive;
+                    _imageTutorialPrompt.enabled = _currentPrompt.IsActive;
+
+                    _listTutorialPrompts.RemoveAt(0);   //Removes said Prompt object from the list                   
+                    ListToQueueTransfer(_listTutorialPrompts, _queueTutorialPrompts);   //Updates the Queue
+
+                    //Updates the _currentPrompt to the first Prompt in queue
                     if (_queueTutorialPrompts.Count != 0)
                     {
                         _currentPrompt = _queueTutorialPrompts.Peek();
@@ -257,6 +353,13 @@ public class TutorialPrompt : MonoBehaviour
         {
             this.enabled = false;
         }
+
+        //DEBUG
+        if (true)
+        {
+            Debug.DrawRay(_currentPrompt.StartTrigger.transform.position, Player.transform.position, Color.red);
+            Debug.DrawRay(Player.transform.position, Player.transform.forward, Color.green);
+        }
     }
 
     // OnTrigger code
@@ -272,8 +375,8 @@ public class TutorialPrompt : MonoBehaviour
                 _textTutorialPrompt.text = _currentPrompt.Message;
             }
 
-            //Sets the Prompt object Ischieved to false when touches the EndTrigger
-            if (other == _currentPrompt.EndTrigger)
+            //Sets the Prompt object IsAchieved to false when touches the EndTrigger
+            if (other == _currentPrompt.EndTrigger && _currentPrompt.IsActive)
             {
                 _currentPrompt.IsActive = false;
                 _currentPrompt.IsAchieved = true;
@@ -281,7 +384,7 @@ public class TutorialPrompt : MonoBehaviour
 
             _listTutorialPrompts[0] = _currentPrompt;
             ListToQueueTransfer(_listTutorialPrompts, _queueTutorialPrompts);
-        }   
+        }      
     }
 
     /// <summary>
