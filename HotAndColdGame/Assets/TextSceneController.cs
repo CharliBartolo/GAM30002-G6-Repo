1fdf10196cda6d4dd5e45df4f80f2409299fcf4c
Charli <charlib33@gmail.com>
2021-10-02 17:13:56 +1000

    Credits now exit themselves 3 seconds after reaching end

--- HotAndColdGame/Assets/TextSceneController.cs
+++ HotAndColdGame/Assets/TextSceneController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class TextSceneController : MonoBehaviour
 {
+    public Animator textAnimator;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +15,12 @@ public class TextSceneController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (textAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1)
+        {
+            Debug.Log("Skipping in 3s");
+            Invoke("Skip", 3f);
+            
+        }
     }
 
     public void Skip()
