da9ff4df3a42ec4fd0b538e6590ef9b974aa5d8f
Matt Smith <102088891@student.swin.edu.au>
2021-10-09 17:46:48 +1100

    update player sound

--- HotAndColdGame/Assets/Scripts/PlayerSoundControl.cs
+++ HotAndColdGame/Assets/Scripts/PlayerSoundControl.cs
@@ -26,7 +26,7 @@ public class PlayerSoundControl : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        playerAudio = Camera.main.GetComponent<AudioSource>();
+        playerAudio = Camera.main.transform.Find("AudioPosition").GetComponent<AudioSource>();
 
         raygunAudio = GetComponent<GunFXController>().gun_obj.GetComponent<AudioSource>();
         activeConditions = new List<IConditions.ConditionTypes>();
