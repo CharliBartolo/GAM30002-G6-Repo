67631a69f81610f9ef5c2a8a74b4463db65192c7
Matt Smith <102088891@student.swin.edu.au>
2021-10-16 14:55:08 +1100

    update  lvl2, raygun now counted as artifact; update camControl player ref issue

--- HotAndColdGame/Assets/Scripts/Interactables/CollectInteractable.cs
+++ HotAndColdGame/Assets/Scripts/Interactables/CollectInteractable.cs
@@ -69,6 +69,8 @@ public class CollectInteractable : InteractableBase
                 break;
         }
 
+        GameMaster.instance.GetComponent<CollectionSystem>().FoundCollectable(this);
+
         if (destroyOnCollect)
             Destroy(gameObject);
     }
